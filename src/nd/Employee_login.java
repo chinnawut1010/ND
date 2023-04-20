05634a6b9891afc15fbcbc57ba7e9b4f2c03cdbb
TimWorld8 <115977838+TimWorld8@users.noreply.github.com>
2023-04-20 14:22:10 +0700

    test02

--- src/nd/Employee_login.java
+++ src/nd/Employee_login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nd;
+package nd;///Brid
 
 import java.awt.Frame;
 import java.sql.Connection;
